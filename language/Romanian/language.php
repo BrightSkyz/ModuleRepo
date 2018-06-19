a7aed06298a1490b3966ce4990451c006171f9ca
Eftimie Andrei <bax@baxandrei.ro>
2018-06-19 16:47:31 +0300

    Create language.php

---
+++ language/Romanian/language.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *	Made by BrightSkyz
+ *  https://github.com/brightskyz
+ *  NamelessMC version 2.0.0-pr4
+ *
+ *  License: MIT
+ *
+ *  Romanian Language for the ModuleRepo module for NamelessMC
+ *  Translation By @BaxAndrei ( https://baxandrei.ro )
+ *  Last Update: 19/06/2018
+ */
+// Which version of NamelessMC is this language file updated to?
+$language_version = '2.0.0-pr4';
+ 
+$language = array(
+	/*
+	 *  General terms
+	 */ 
+	'modulerepo_title' => 'ModuleRepo',
+	'module_link' => 'Adresă modul',
+	'delete' => 'Șterge',
+	'update' => 'Actualizați',
+	'error' => 'Eroare',
+	'no_update_available' => 'Nu este disponibilă nicio actualizare.',
+	'cancel_install' => 'Anulați instalarea',
+	'install_module' => 'Instalați modulul',
+	'module_deleted' => 'Modulul a fost șters.',
+	'repository_server' => 'Server librărie',
+	'are_you_sure_install' => 'Sigur doriți să instalați <strong>{x}@{x2}</strong> de pe <strong>{x3}</strong>?', // {x} = Module name, {x2} = Module version, {x3} = Module server
+	'make_modules_writeable' => 'Permiteți scrierea directorului <strong>modules</strong> folosind comanda <code>chmod -R 775 modules</code> în directorul de instalare NamelessMC.',
+	'installed_with_modulerepo' => 'Acest modul a fost instalat folosind ModuleRepo.',
+	'install_via_modulerepo' => '<i class="fa fa-plus-circle"></i> Instalați folosind ModuleRepo',
+	'module_installed_via_modulerepo' => 'Modulul a fost instalat cu succes folosind ModuleRepo.',
+	'module_updated_via_modulerepo' => 'Modulul a fost actualizat cu succes folosind ModuleRepo.',
+	'zip_extraction_failed' => 'Extragerea zipului a eșuat.',
+	'zip_download_failed' => 'Descărcarea zipului nu a reușit. Sigur există modulul specificat?',
+	'module_fetch_failed' => 'Preluarea zipului nu a reușit. Sigur există modulul specificat?'
+);
